b19f4d11b017188fcc112b37ffca2a68dfe1737b
Luke Zink <83885670+lukezink@users.noreply.github.com>
2022-02-22 04:37:05 +0000

    range and added variables

--- main.py
+++ main.py
@@ -27,10 +27,9 @@ michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
 ## Race 2
-x = 0
 for x in range(0,10):
-  michMove = random.randint(1,20)
-  leoMove = random.randint(1,20)
+  michMove = random.randint(0,11)
+  leoMove = random.randint(0,11)
   michelangelo.forward(michMove)
   leonardo.forward(leoMove)
 michelangelo.goto(-100,20)
@@ -41,9 +40,10 @@ michelangelo.down()
 for x in range (3,13):
   if x == 3 or x == 4 or x == 6 or x==9 or x==12:
     for y in range (0,x):
+      angle = 360/x
       michelangelo.color('orange')
-      michelangelo.forward(30)
-      michelangelo.right(360/x)
+      michelangelo.forward(angle)
+      michelangelo.right(angle)
     michelangelo.clear()
   
 window.exitonclick()
